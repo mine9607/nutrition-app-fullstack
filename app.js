8084a0b182afac2226da9206cc040dc62eb92719
mine9607 <81383389+mine9607@users.noreply.github.com>
2023-08-09 17:11:51 -0500

    Added basic layouts for login and register routes

--- app.js
+++ app.js
@@ -8,9 +8,33 @@ import _ from "lodash";
 const app = express();
 const port = 3000;
 
+//set the root for static files at the "public" directory
+app.use(express.static("public"));
+//allows grabbing body inputs from forms
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
   res.render("home.ejs");
 });
+
+app
+  .route("/login")
+  .get((req, res) => {
+    res.render("login.ejs");
+  })
+  .post((req, res) => {
+    console.log(req.body.email);
+    console.log(req.body.password);
+  });
+app
+  .route("/register")
+  .get((req, res) => {
+    res.render("register.ejs");
+  })
+  .post((req, res) => {
+    console.log(req.body.email);
+    console.log(req.body.password);
+  });
 
 app.listen(port, () => {
   console.log(`Server is listening on port: ${port}`);
